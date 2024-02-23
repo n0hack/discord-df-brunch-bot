a4446cc0c6393476f24d6497f488c5c62928a6b5
Lucid <nohack-@naver.com>
2024-02-23 19:14:56 +0900

    [chore] 무중단 배포 관련 함수 주석 추가

--- src/healthCheck.ts
+++ src/healthCheck.ts
@@ -1,5 +1,8 @@
 import http from 'http';
 
+/**
+ * koyeb 클라우드 무중단 배포를 위한 체크 함수
+ */
 export const healthCheck = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.write('OK');
